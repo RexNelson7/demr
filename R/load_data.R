0ada709a7c5a94cb78d9246624284110dfb3ca99
Grant Cowles <gycowles05@gmail.com>
2026-05-23 15:31:29 -0700

    Initial commit

---
+++ R/load_data.R
@@ -0,0 +1,33 @@
+load_data <- function(){
+  tuesdata <- tidytuesdayR::tt_load('2024-11-05')
+  tuesdata$democracy_data
+}
+
+library(roxygen2)
+
+#'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

bf50c2739282bf5d5759cb03491f8adcf788fadb
egregi0us <gycowles05@gmail.com>
2026-05-23 16:02:57 -0700

    Add files via upload

---
+++ load_data.R
@@ -0,0 +1,36 @@
+load_data <- function(){
+  tuesdata <- tidytuesdayR::tt_load('2024-11-05')
+  tuesdata$democracy_data
+}
+
+library(roxygen2)
+
+#' This is just to load our preliminary data.
+#' @importFrom tidytuesdayR tt_load
+#'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

816995468e45062df0d80575a308e18a4a467f80
RexNelson7 <rnelso33@calpoly.edu>
2026-05-30 22:04:42 -0700

    proj3week2

--- R/load_data.R
+++ R/load_data.R
@@ -1,19 +1,18 @@
-library(roxygen2)
-
-#' This is the first function to load our data.
+#' Load democracy data
 #'
-#' @importFrom readr read_csv
+#' Loads the democracy data used in the demr package.
 #'
-#'
-#' @return A data frame from dem_data.csv
-#'
+#' @return A tibble containing country-level democracy and representation data.
 #' @export
+load_data <- function() {
+  data_path <- system.file("extdata", "dem_data.csv", package = "demr")
 
-load_data <- function(){
-  demdata <- read_csv("dem_data.csv")
+  if (data_path == "") {
+    stop("Could not find dem_data.csv in inst/extdata.", call. = FALSE)
+  }
+
+  readr::read_csv(data_path, show_col_types = FALSE)
 }
-
-
 
 
 
